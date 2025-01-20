726aa5188ea25524f27fa0f0d8fc68248fcd0f03
Nina Schrauwen <48436317+NceSchrauwen@users.noreply.github.com>
2025-01-20 15:16:22 +0100

    Create opdr1_first_script.py

---
+++ opdr1_first_script.py
@@ -0,0 +1,5 @@
+# Opdracht 1: Hello World
+# Name: Nina Schrauwen
+# Description: Test the SSH connection and run a Python script on the Raspberry Pi
+
+print("First Python script successfully ran by the pi!")
