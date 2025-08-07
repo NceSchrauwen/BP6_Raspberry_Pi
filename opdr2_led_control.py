c52b96d92d1b40556fa08e90b11e3aa6dfb1f18f
Nina Schrauwen <48436317+NceSchrauwen@users.noreply.github.com>
2025-08-07 17:02:58 +0200

    Update opdr2_led_control.py

--- opdr2_led_control.py
+++ opdr2_led_control.py
@@ -1,41 +1,42 @@
-# Opdracht 2: LED control
-# Name: Nina Schrauwen
-# Description: Control a LED with at differing blinking intervals
+# Opdracht 2: LED aansturing
+# Naam: Nina Schrauwen
+# Omschrijving: Stuur een LED aan met verschillende knipperintervallen
 
-#Import libraries
+# Importeren van bibliotheken
 import RPi.GPIO as GPIO
 import time
 
-# GPIO pin number of LED
+# GPIO pin nummer van de LED
 LED = 18
 
 # GPIO setup
-GPIO.setmode(GPIO.BCM) # Use Broadcom SOC channel numbering
-GPIO.setup(LED, GPIO.OUT) # Set pin as output
+GPIO.setmode(GPIO.BCM) # Gebruik Broadcom SOC kanaalnummering
+GPIO.setup(LED, GPIO.OUT) # Zet pin als uitgang
 
-# Function to blink LED at given interval
-def blink_led(speed_on, speed_off, repetitions):
-    for _ in range(repetitions):
-        GPIO.output(LED, GPIO.HIGH) # Turn on LED
-        time.sleep(speed_on) # Duration of on interval
-        GPIO.output(LED, GPIO.LOW) # Turn off LED
-        time.sleep(speed_off) # Duration of off interval
+# Functie om LED te laten knipperen met opgegeven interval
+def blink_led(snelheid_aan, snelheid_uit, herhalingen):
+    for _ in range(herhalingen):
+        GPIO.output(LED, GPIO.HIGH) # Zet LED aan
+        time.sleep(snelheid_aan) # Duur van het aan-interval
+        GPIO.output(LED, GPIO.LOW) # Zet LED uit
+        time.sleep(snelheid_uit) # Duur van het uit-interval
 
-# Blink LED at different intervals
+# Laat LED knipperen met verschillende intervallen door de blink_led() functie aan te roepen
 try:
-    print("Opdracht a: Blink speed is 1 second on, 1 second off.")
+    print("Opdracht a: Knippersnelheid is 1 seconde aan, 1 seconde uit.")
     blink_led(1, 1, 5)
 
-    print("Opdracht b: Blink speed is 1 second on, 2 seconds off.")
+    print("Opdracht b: Knippersnelheid is 1 seconde aan, 2 seconden uit.")
     blink_led(1, 2, 5)
 
-    print("Opdracht c: Blink speed is 0.1 seconds on, 0.1 seconds off.")
+    print("Opdracht c: Knippersnelheid is 0.1 seconde aan, 0.1 seconde uit.")
     blink_led(0.1, 0.1, 25)
 
-    print("Opdracht d: Giving the illusion of being always on because of the short blinking interval.")
+    print("Opdracht d: De illusie wekken dat de LED altijd aan is door het korte knipperinterval.")
     blink_led(0.01, 0.01, 100)
 
-# Clean up GPIO settings
+# Maak de GPIO-instellingen schoon
 finally:
     GPIO.cleanup()
-    print("GPIO cleanup completed.")
+    print("GPIO schoonmaak voltooid.")
+    
