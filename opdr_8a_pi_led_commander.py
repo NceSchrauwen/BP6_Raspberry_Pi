3a79cb316a24be12ed9d3bbaa513e8168530152e
Nina Schrauwen <48436317+NceSchrauwen@users.noreply.github.com>
2025-08-07 14:57:04 +0200

    Update opdr_8a_pi_led_commander.py

--- opdr_8a_pi_led_commander.py
+++ opdr_8a_pi_led_commander.py
@@ -1,21 +1,22 @@
 # Opdracht 8a Raspberry Pi - Nina Schrauwen
-# LED1 and LED2 have to flash alternately with a 1 second interval.
-# This program is to command the arduino to power the LEDs when they need to be on/off.
+# LED1 en LED2 moeten afwisselend knipperen met een interval van 1 seconde.
+# Dit programma stuurt de Arduino aan om de LEDs aan of uit te zetten wanneer dat nodig is.
 
 import serial
 import time
 
-# Serial communication with Arduino with baudrate 9600
+# Seriële communicatie met de Arduino met een baudrate van 9600
 ser = serial.Serial('/dev/serial0', 9600)
 time.sleep(2)
 
 print("Seriële verbinding met Arduino is tot stand gebracht.")
 
-# Main loop to command the Arduino
+# Hoofdlus om de Arduino aan te sturen
 while True:
-    ser.write(b'A')  # Turn on LED1, turn off LED2
+    ser.write(b'A')  # LED1 aanzetten, LED2 uitzetten
     print("LED1 aan, LED2 uit")
     time.sleep(1)
-    ser.write(b'B')  # Turn off LED1, turn on LED2
+    ser.write(b'B')  # LED1 uitzetten, LED2 aanzetten
     print("LED1 uit, LED2 aan")
     time.sleep(1)
+    
